83e75be70d04248afc7ceb11225f786ce97eabcc
Jeremy Auvray <jeremy93-2008@hotmail.fr>
2022-08-25 17:14:48 +0200

    Initial Commit

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function App() {
+    return <div className="flex justify-center items-center bg-slate-800 h-[100vh]">
+        <div className="bg-slate-500 w-[40vw] h-[40vh] px-5 py-3">
+            Hola mundo
+        </div>
+    </div>
+}
